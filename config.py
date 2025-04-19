fad9a33af7a655e40c0b60bebb7f01e876490412
chiayikuang <chiayk-wm23@student.tarc.edu.my>
2025-04-19 18:28:14 +0800

    config.py

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ customhost = "employee.c5iqv0cplycb.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
 custompass = "Bait3273"
 customdb = "employee"
-custombucket = "lowchoonkeat-employee1"
+custombucket = "chiayikuang"
 customregion = "us-east-1"
 
